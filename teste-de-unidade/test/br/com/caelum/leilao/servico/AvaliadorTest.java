1dc558dee1423ecc39e98f6b8351f15813d00ddd
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-06-03 10:06:34 -0300

    Aula 5 - Testando exceções

--- teste-de-unidade/test/br/com/caelum/leilao/servico/AvaliadorTest.java
+++ teste-de-unidade/test/br/com/caelum/leilao/servico/AvaliadorTest.java
@@ -1,5 +1,8 @@
 package br.com.caelum.leilao.servico;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
@@ -58,12 +61,8 @@ public class AvaliadorTest {
 		// executando a acao
 		leiloeiro.avalia(leilao);
 
-		// comparando a saida com o esperado
-		double maiorEsperado = 400;
-		double menorEsperado = 250;
-
-		assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.0001);
-		assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.0001);
+        assertThat(leiloeiro.getMenorLance(), equalTo(250.0));
+        assertThat(leiloeiro.getMaiorLance(), equalTo(400.0));
 	}
 
 	@Test
@@ -76,9 +75,7 @@ public class AvaliadorTest {
 
         leiloeiro.avalia(leilao);
 
-		// veja que não precisamos mais da palavra Assert!
-		assertEquals(1000.0, leiloeiro.getMaiorLance(), 0.0001);
-		assertEquals(1000.0, leiloeiro.getMenorLance(), 0.0001);
+        assertThat(leiloeiro.getMenorLance(), equalTo(leiloeiro.getMaiorLance()));
 	}
 
 	@Test
@@ -96,8 +93,21 @@ public class AvaliadorTest {
 
 		List<Lance> maiores = leiloeiro.getTresMaiores();
 		assertEquals(3, maiores.size());
-		assertEquals(400.0, maiores.get(0).getValor(), 0.00001);
-		assertEquals(300.0, maiores.get(1).getValor(), 0.00001);
-		assertEquals(200.0, maiores.get(2).getValor(), 0.00001);
+		
+		assertThat(maiores, hasItems(
+				new Lance(maria,400),
+				new Lance(joao,300),
+				new Lance(maria,200)
+				));
 	}
+	
+	@Test(expected=RuntimeException.class)
+	public void naoDeveAvaliarLeiloesSemNenhumLanceDado() {
+	    Leilao leilao = new CriadorDeLeilao()
+	        .para("Playstation 3 Novo")
+	        .constroi();
+
+	    leiloeiro.avalia(leilao);
+	}
+	
 }
